6cffd4fd35026cfb7bf552cff9e78c11f4afe412
cheomji <0311bom@naver.com>
2022-05-25 03:57:12 +0900

    complete

---
+++ 0530 study/Project2/10870.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int fibo(int n) {
+	if (n == 0) return 0;
+	else if (n == 1) return 1;
+	else return fibo(n - 1) + fibo(n - 2);
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d", fibo(n));
+	return 0;
+}
